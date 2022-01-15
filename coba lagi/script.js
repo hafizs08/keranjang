bf2a3da3d7f4788efcf52ee636254306a39b9dbc
hafizs08 <94980822+hafizs08@users.noreply.github.com>
2022-01-16 00:10:32 +0700

    upload file
    ulang lagi

---
+++ coba lagi/script.js
@@ -0,0 +1,57 @@
+$(document).ready(function(){
+    update_amounts();
+    $('.qty, .price').on('keyup keypress blur change', function(e) {
+        update_amounts();
+    });
+});
+function update_amounts(){
+    var sum = 0.0;
+    $('#myTable > tbody  > tr').each(function() {
+        var qty = $(this).find('.qty').val();
+        var price = $(this).find('.price').val();
+        var amount = (qty*price)
+        sum+=amount;
+        $(this).find('.amount').text(''+amount);
+    });
+    $('.total').text(sum);
+    $('.totalB').text();
+}
+
+
+//----------------for quantity-increment-or-decrement-------
+var incrementQty;
+var decrementQty;
+var plusBtn  = $(".cart-qty-plus");
+var minusBtn = $(".cart-qty-minus");
+var incrementQty = plusBtn.click(function() {
+    var $n = $(this)
+    .parent(".button-container")
+    .find(".qty");
+    $n.val(Number($n.val())+1 );
+    update_amounts();
+});
+var decrementQty = minusBtn.click(function() {
+    var $n = $(this)
+    .parent(".button-container")
+    .find(".qty");
+    var QtyVal = Number($n.val());
+    if (QtyVal > 0) {
+        $n.val(QtyVal-1);
+    }
+    update_amounts();
+});
+
+//select all
+var checkboxes = document.querySelectorAll("input[type = 'checkbox']");
+function checkAll(myCheckbox){
+    if(myCheckbox.checked == true){
+        checkboxes.forEach(function(checkbox){
+            checkbox.checked = true;
+        });
+    }
+    else{
+        checkboxes.forEach(function(checkbox){
+            checkbox.checked = false;
+        });
+    }
+}
